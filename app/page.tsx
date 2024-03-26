a2242566abfa3160a95b442ebaa9cb4b918310f9
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-03-26 18:51:49 +0100

    refactor: copywriting

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,20 @@ export default function Home() {
     <div className="flex flex-col  min-h-full relative overflow-hidden justify-center">
         <Image priority={true} src="/waves.png" alt="waves background" width={1578.76} height={1021.86} className="hidden sm:block -rotate-45 absolute -z-10 -right-72 top-48"></Image>
         <h1 className="text-5xl">
-          Next <strong>auditing</strong> <br />
-          platform <br />
-          for your <strong>code !</strong>
+          Have you ever wonder<br />
+           if your code was<br />
+           <strong>safe?</strong>
         </h1>
         <p className="mt-6">
-          Your go-to marketplace for open-source audits and code certification. <br />
-          Bringing developers together for <strong>secure</strong>, <strong>certified</strong> solutions!
+          CodeShield is the place where you can find <br/>
+          the best auditors to certify your code.
         </p>
         <div className="flex gap-6 mt-2">
-          <Link href="login">
-            <Button >Enroll as an auditor</Button>
+          <Link href="publish">
+            <Button >Ask for feedback</Button>
           </Link>
-          <Link href="auditors">
-            <Button variant="link">Need to certify your code?</Button>
+          <Link href="login">
+            <Button variant="link">Enroll as a certified auditor now</Button>
           </Link>
       </div>
     </div>
