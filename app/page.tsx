1318df2365ae9e64b3a5c64aec17b26354096f14
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-03-26 19:08:18 +0100

    feat: links and styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,19 @@ export default function Home() {
         <Image priority={true} src="/waves.png" alt="waves background" width={1578.76} height={1021.86} className="hidden sm:block -rotate-45 absolute -z-10 -right-72 top-48"></Image>
         <h1 className="text-5xl">
           Have you ever wonder<br />
-           if your code was<br />
+           if software was<br />
            <strong>safe?</strong>
         </h1>
         <p className="mt-6">
           CodeShield is the place where you can find <br/>
           the best auditors to certify your code.
         </p>
-        <div className="flex gap-6 mt-2">
+        <div className="flex gap-x-6 mt-2 flex-wrap">
           <Link href="publish">
             <Button >Ask for feedback</Button>
           </Link>
-          <Link href="login">
-            <Button variant="link">Enroll as a certified auditor now</Button>
+          <Link href="profile">
+            <Button variant="link">Enroll as a certified auditor</Button>
           </Link>
       </div>
     </div>
