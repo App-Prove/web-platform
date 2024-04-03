953daffd89c039e43b72661b9cd9476093d2f450
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-04-03 16:32:31 +0200

    feat: dropdown close on select

--- components/authButton.tsx
+++ components/authButton.tsx
@@ -30,14 +30,14 @@ export default  function AuthButton({user}:{user:any}) {
                     <DropdownMenuContent>
                         <DropdownMenuLabel>Account</DropdownMenuLabel>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem><Link href='profile'>Profile</Link></DropdownMenuItem>
-                        <DropdownMenuItem><Link href='billing'>Billing</Link></DropdownMenuItem>
+                        <DropdownMenuItem asChild><Link href='profile'>Profile</Link></DropdownMenuItem>
+                        <DropdownMenuItem asChild><Link href='billing'>Billing</Link></DropdownMenuItem>
                         <DropdownMenuLabel>Navigation</DropdownMenuLabel>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem><Link href='auditors'>Auditors</Link></DropdownMenuItem>
-                        <DropdownMenuItem><Link href='offers'>Offers</Link></DropdownMenuItem>
-                        <DropdownMenuItem><Link href='publish'>Publish an offer</Link></DropdownMenuItem>
-                        <DropdownMenuItem><form><Button formAction={logout} variant="destructive">Sign Out</Button></form></DropdownMenuItem>
+                        <DropdownMenuItem asChild><Link href='auditors'>Auditors</Link></DropdownMenuItem>
+                        <DropdownMenuItem asChild><Link href='offers'>Offers</Link></DropdownMenuItem>
+                        <DropdownMenuItem asChild><Link href='publish'>Publish an offer</Link></DropdownMenuItem>
+                        <DropdownMenuItem asChild><form><Button formAction={logout} variant="destructive">Sign Out</Button></form></DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
 
