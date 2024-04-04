b708464b79ab929097ed131fd7be8f607ffd24ac
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-04-04 16:33:05 +0200

    feat: removing toast

--- components/publish-form.tsx
+++ components/publish-form.tsx
@@ -132,14 +132,14 @@ export default function PublishForm() {
 
     function onSubmit(data: z.infer<typeof FormSchema>) {
         console.log(data)
-        toast({
-            title: "You submitted the following values:",
-            description: (
-                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-                </pre>
-            ),
-        })
+        // toast({
+        //     title: "You submitted the following values:",
+        //     description: (
+        //         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+        //             <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        //         </pre>
+        //     ),
+        // })
 
         postFormToDB(data)
     }
