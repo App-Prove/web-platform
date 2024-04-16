7544f64403d2f4de869a5c526e07e795807e95f6
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-04-16 15:27:11 +0200

    fix: iterator issue due to missing key

--- app/offers/loading.tsx
+++ app/offers/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
         <div className="w-full flex flex-col gap-4">
             {Array.from({ length: 5 }).map((_, i) => {
                 return(
-            <Skeleton className="w-full h-40">
+            <Skeleton className="w-full h-40" key={i}>
             </Skeleton>
                 )
             })}
