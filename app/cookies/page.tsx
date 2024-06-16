303e5d9707ff412794edfeee3e26a5e4ad361267
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-06-16 14:15:49 +0200

    feat: new pages

---
+++ app/cookies/page.tsx
@@ -0,0 +1,9 @@
+export default function CookiesPage() {
+    return (
+        <div className='h-full w-full mx-auto flex flex-col gap-4 mb-16 overflow-visible'>
+            <h1>
+                Cookies
+            </h1>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -89,8 +89,8 @@ export default function Home() {
             </div>
           </h3>
           <div className="self-center justify-center flex overflow-visible flex-wrap sm:justify-start">
-          <SecurityCertification></SecurityCertification>
-          <ReliabilityCertification></ReliabilityCertification>
+            <SecurityCertification></SecurityCertification>
+            <ReliabilityCertification></ReliabilityCertification>
           </div>
         </div>
         <Separator />
@@ -193,6 +193,22 @@ export default function Home() {
             </AccordionItem>
           </Accordion>
         </div>
+        <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+          Trusted by the biggest European incubator
+        </h2>
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="/euratechnologies.png"
+            alt="Euratechnologies"
+            width={158}
+            height={48}
+          />
+        </div>
+      </div>
+    </div>
         <div className="flex flex-col justify-center items-center">
           <h2 className={cn("font-medium text-3xl", whyte.className)}>Try our platform now!</h2>
           <p className="text-center text-muted-foreground">
