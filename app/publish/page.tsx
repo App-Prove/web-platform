f4e9680309ca8ac08f1bb295cc97bea4edf8d9d2
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-06-16 13:00:28 +0200

    feat: making publisd page private

--- app/publish/page.tsx
+++ app/publish/page.tsx
@@ -1,19 +1,14 @@
 import * as React from "react"
-
 import { redirect } from 'next/navigation'
 import PublishForm from "@/components/Publish/Form"
 import { createClient } from "@/utils/supabase/server"
-import { CheckoutForm } from "@/components/checkout"
-
-
 
 export default async function PrivatePage() {
-    // const supabase = createClient()
-    // const { data, error } = await supabase.auth.getUser()
-    // if (error || !data?.user) {
-    //     redirect('/?error=unauthorized')
-    // }
-
+    const supabase = createClient()
+    const { data, error } = await supabase.auth.getUser()
+    if (error || !data?.user) {
+        redirect('/?error=unauthorized')
+    }
     return (
         <div className='h-full w-full mx-auto flex flex-col gap-4 mb-16 overflow-visible'>
             <PublishForm></PublishForm>
