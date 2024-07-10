339ea3bbad606193a62b4da74f781cb6853f891f
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-07-10 20:27:59 +0200

    chore: Update Contact page layout and content
    Refactor the Contact page to improve its layout and content. This commit removes unnecessary elements and updates the text to provide a better user experience. The changes include:
    - Updating the layout to be more responsive
    - Removing the bug reports section
    - Adding a new section for technical support with a message inviting users to join the Discord server for assistance

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,14 +2,8 @@ import { BugAntIcon, ChatBubbleLeftRightIcon, ComputerDesktopIcon } from '@heroi
 
 export default function Contact() {
     return (
-    <div className="mx-auto isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="mx-auto max-w-2xl sm:text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
-        </p>
-      </div>
-      <div className="mx-auto mt-20 max-w-lg space-y-16">
+    <div className="w-full text-left isolate bg-white py-12 sm:py-4 ">
+      <div className="mt-20 max-w-lg space-y-16">
         <div className="flex gap-x-6">
           <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-orange">
             <ChatBubbleLeftRightIcon className="h-6 w-6 text-white" aria-hidden="true" />
@@ -28,28 +22,12 @@ export default function Contact() {
         </div>
         <div className="flex gap-x-6">
           <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-orange">
-            <BugAntIcon className="h-6 w-6 text-white" aria-hidden="true" />
-          </div>
-          <div>
-            <h3 className="text-base font-semibold leading-7 text-gray-900">Bug reports</h3>
-            <p className="mt-2 leading-7 text-gray-600">
-              Expedita qui non ut quia ipsum voluptatum ipsam pariatur. Culpa vitae ipsum minus eius vero quo quibusdam.
-            </p>
-            <p className="mt-4">
-              <a href="#" className="text-sm font-semibold leading-6 text-orange">
-                Report a bug <span aria-hidden="true">&rarr;</span>
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-x-6">
-          <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-orange">
             <ComputerDesktopIcon className="h-6 w-6 text-white" aria-hidden="true" />
           </div>
           <div>
             <h3 className="text-base font-semibold leading-7 text-gray-900">Technical support</h3>
             <p className="mt-2 leading-7 text-gray-600">
-              Sint aut modi porro consequatur architecto commodi qui consequatur. Dignissimos adipisci minima.
+            For any technical issue, feel free to join our Discord server. We are here to help you.
             </p>
             <p className="mt-4">
               <a href="#" className="text-sm font-semibold leading-6 text-orange">
