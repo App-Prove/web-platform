0ac31159df5cdfd20e1d095d225c21ab99cbfa5d
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-07-26 20:10:35 +0200

    refactor: adding different pricing, and pricing page
    Link with Stripe pricings, and getting pricing mode during offer publishing

--- app/stripe/create-checkout-session/route.tsx
+++ app/stripe/create-checkout-session/route.tsx
@@ -14,48 +14,35 @@ export async function POST(request: Request) {
   }
   // Get id from post data
   // Identify promo code
-  const { id, affiliate } = await request.json();
+  const { id, affiliate, pricing } = await request.json();
   console.log("ID is  for metadata",id)
   affiliate && console.log('Affiliate found')
+  console.log('Pricing is',pricing)
   // Get offer data from id
   // Get budget from id of the offer
+  const stripePayload = {
+    ui_mode: 'embedded',
+    customer_email: user?.email,
+    line_items: [
+      {
+        price: pricing=='one-time-audit'?process.env.STRIPE_PRICE_ID_ONETIME:process.env.STRIPE_PRICE_ID,
+        quantity: 1,
+      }
+    ],
+    metadata: {
+      id: id
+    },
+    mode: pricing==='one-time-audit'?'payment':'subscription',
+    return_url: `${getWebsiteURL()}/publish/payment/processed?session_id={CHECKOUT_SESSION_ID}`,
+  }
   try {
-    const session = await stripe.checkout.sessions.create({
-      ui_mode: 'embedded',
-      customer_email: user?.email,
-      line_items: [
-        {
-          price: process.env.STRIPE_PRICE_ID,
-          quantity: 1,
-        }
-      ],
-      metadata: {
-        id: id
-      },
-      mode: 'subscription',
-      return_url: `${getWebsiteURL()}/publish/payment/processed?session_id={CHECKOUT_SESSION_ID}`,
-      ...(affiliate ? { discounts: [{ coupon: affiliate }] } : {}),
-    });
+    const session = await stripe.checkout.sessions.create({...stripePayload, ...(affiliate ? { discounts: [{ coupon: affiliate }] } : {})});
     return NextResponse.json({
       clientSecret: session.client_secret
     });
   } catch (error) {
       // Handle StripeInvalidRequestError specifically
-      const session = await stripe.checkout.sessions.create({
-        ui_mode: 'embedded',
-        customer_email: user?.email,
-        line_items: [
-          {
-            price: process.env.STRIPE_PRICE_ID,
-            quantity: 1,
-          }
-        ],
-        metadata: {
-          id: id
-        },
-        mode: 'subscription',
-        return_url: `${getWebsiteURL()}/publish/payment/processed?session_id={CHECKOUT_SESSION_ID}`,
-      });
+      const session = await stripe.checkout.sessions.create(stripePayload);
       return NextResponse.json({
         clientSecret: session.client_secret
       });
